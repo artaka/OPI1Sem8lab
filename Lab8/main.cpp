f787ed57f668569c51a6bd6bcbfd55114af0047c
artaka <fefsert@gmail.com>
2024-10-28 15:26:38 +0300

    Зачтено

---
+++ Lab8/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Header.h"
+using namespace std;
+
+int main() {
+	int param1 = 5;
+	for (int i = 0;i < 5;i++) {
+		int param2 = param1 + i;
+		int result_getSum = getSum(param1, param2);
+		cout << "i= " << i << ' ';
+		cout << "getSum(" << param1 << "," << param2 << ") = " << result_getSum << endl;
+	}
+	int result_getMul = getMul(2, 3);
+	cout << result_getMul << endl;
+	system("pause");
+}
